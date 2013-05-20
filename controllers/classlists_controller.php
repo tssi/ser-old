a2615e824bcf675dab314d436d5a38587d559492
Garry M. Perez <perez.garrym@gmail.com>
2013-05-20 17:42:45 +0800

    Classlists and students sanitize data

--- controllers/classlists_controller.php
+++ controllers/classlists_controller.php
@@ -4,8 +4,29 @@ class ClasslistsController extends AppController {
 	var $name = 'Classlists';
 
 	function index() {
-		$this->Classlist->recursive = 0;
-		$this->set('classlists', $this->paginate());
+		if($this->Rest->isActive()){
+			if(isset($_GET)){
+				$Classlists = $this->api($_GET);
+			}
+		}else if($this->RequestHandler->isAjax()){	
+			$this->Classlist->recursive = 2;
+			$cond = array();
+			if(!empty($this->data)){
+				foreach($this->data['Classlist'] as $field=>$value){
+					$cond['Classlist.'.$field]=$value;
+				}
+				
+			}
+			$Classlists  = $this->Classlist->find('all',array('conditions'=>$cond,'limit'=>20));
+		}else{
+			$this->Classlist->recursive = 0;
+			$this->set('Classlists', $this->paginate());
+		}
+		if($this->Rest->isActive()||$this->RequestHandler->isAjax()){
+			//Sanitize data
+			echo json_encode($Classlists);
+			exit;
+		}
 	}
 
 	function view($id = null) {
@@ -63,5 +84,53 @@ class ClasslistsController extends AppController {
 		}
 		$this->Session->setFlash(__('Classlist was not deleted', true));
 		$this->redirect(array('action' => 'index'));
+	}
+	
+	protected function api($params){
+		$schema = $this->Classlist->schema();
+		//pr($schema);
+		$conditions = array();
+		$fields = array();
+		$group = array();
+		foreach($params as $key => $val){
+			switch($key){
+				case 'fields':
+					foreach(explode(',',$val) as $f){
+						if(isset($schema[$f])){
+							array_push($fields,'Classlist.'.$f);
+						}else{
+							return $this->Rest->abort(array('status' => '404', 'error' => 'Invalid field '.$f.' supplied'));
+						}
+					}
+				break;
+				case 'group':
+					foreach(explode(',',$val) as $f){
+						if(isset($schema[$f])){
+							array_push($group,'Classlist.'.$f);
+							if(count($fields)==0){
+								foreach($schema as $sk=>$sv){
+									array_push($fields,'Classlist.'.$sk);									
+								}
+							}
+							if(!in_array('GROUP_CONCAT(Classlist.id) as ids',$fields)){
+								array_push($fields,'GROUP_CONCAT(Classlist.id) as ids');
+							}
+						}else{
+							return $this->Rest->abort(array('status' => '404', 'error' => 'Invalid field '.$f.' supplied'));
+						}
+					}
+					
+				break;
+				default:
+					if(isset($schema[$key])){
+						$conditions['Classlist.'.$key]=$val;
+					}else if($key!='url'){
+						return $this->Rest->abort(array('status' => '404', 'error' => 'Invalid keyword '.$key.' supplied'));
+					}	
+				break;
+			}
+		}
+		$this->Classlist->recursive = 1;
+		return $this->Classlist->find('all',array('conditions'=>$conditions,'group'=>$group,'fields'=>$fields,'order'=>array('Student.gender'=>'DESC')));
 	}
 }
--- controllers/students_controller.php
+++ controllers/students_controller.php
@@ -4,8 +4,50 @@ class StudentsController extends AppController {
 	var $name = 'Students';
 
 	function index() {
-		$this->Student->recursive = 0;
-		$this->set('students', $this->paginate());
+		if($this->Rest->isActive()){
+			if(isset($_GET)){
+				$Students = $this->api($_GET);
+			}
+		}else if($this->RequestHandler->isAjax()){	
+			$this->Student->recursive = 2;
+			$cond = array();
+			if(!empty($this->data)){
+				foreach($this->data['Student'] as $field=>$value){
+					$cond['Student.'.$field]=$value;
+				}
+				
+			}
+			$Students  = $this->Student->find('all',array('conditions'=>$cond));
+		}else{
+			$this->Student->recursive = 0;
+			$this->set('students', $this->paginate());
+		}
+		if($this->Rest->isActive()||$this->RequestHandler->isAjax()){
+			//Sanitize data
+			//pr($Students);exit();
+			foreach($Students as $index=>$data){
+				$classlists = array();
+				foreach($data['Classlist'] as $component){
+					$classlistObj = array();
+					foreach( $component as $field => $value){
+						if(is_array($value)){
+							$classlistObj[$field] = $value;
+						}else{
+							$classlistObj['Classlist'][$field] = $value;
+							
+						}
+					}
+					array_push($classlists,$classlistObj);
+				}
+				$Students[$index]['Classlist'] = $classlists;
+			}
+			if($this->Rest->isActive()){
+				$this->set('data',$Students);
+			}else{				
+				echo json_encode($Students);
+				exit;
+			}
+		}
 	}
 
 	function view($id = null) {
@@ -57,5 +99,57 @@ class StudentsController extends AppController {
 		}
 		$this->Session->setFlash(__('Student was not deleted', true));
 		$this->redirect(array('action' => 'index'));
+	}
+	
+		protected function api($params){
+		$schema = $this->Student->schema();
+		//pr($schema);
+		$conditions = array();
+		$fields = array();
+		$group = array();
+		foreach($params as $key => $val){
+			switch($key){
+				case 'deptcode':
+					$conditions['Student.limit']=$val;
+					$conditions['Student.scope']='D';
+				break;
+				case 'fields':
+					foreach(explode(',',$val) as $f){
+						if(isset($schema[$f])){
+							array_push($fields,'Student.'.$f);
+						}else{
+							return $this->Rest->abort(array('status' => '404', 'error' => 'Invalid field '.$f.' supplied'));
+						}
+					}
+				break;
+				case 'group':
+					foreach(explode(',',$val) as $f){
+						if(isset($schema[$f])){
+							array_push($group,'Student.'.$f);
+							if(count($fields)==0){
+								foreach($schema as $sk=>$sv){
+									array_push($fields,'Student.'.$sk);									
+								}
+							}
+							if(!in_array('GROUP_CONCAT(Student.id) as ids',$fields)){
+								array_push($fields,'GROUP_CONCAT(Student.id) as ids');
+							}
+						}else{
+							return $this->Rest->abort(array('status' => '404', 'error' => 'Invalid field '.$f.' supplied'));
+						}
+					}
+					
+				break;
+				default:
+					if(isset($schema[$key])){
+						$conditions['Student.'.$key]=$val;
+					}else if($key!='url'){
+						return $this->Rest->abort(array('status' => '404', 'error' => 'Invalid keyword '.$key.' supplied'));
+					}	
+				break;
+			}
+		}
+		$this->Student->recursive = 2;
+		return $this->Student->find('all',array('conditions'=>$conditions,'group'=>$group,'fields'=>$fields));
 	}
 }
